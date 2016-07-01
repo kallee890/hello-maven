9c53e1ce3f9d853586b10eb62bb6e57b91961106
kallee890 <kallee890@naver.com>
2016-07-01 10:38:36 +0900

    Third mod

--- src/main/java/kr/co/kosta/hello_maven/App.java
+++ src/main/java/kr/co/kosta/hello_maven/App.java
@@ -4,11 +4,12 @@ package kr.co.kosta.hello_maven;
  * Hello world!
  * GitHub 에서 변경
  * second mod
+ * Third mod
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello JAVA World!!!~~" );
+        System.out.println( "Hello JAVA World!!!~~@@" );
     }
 }
